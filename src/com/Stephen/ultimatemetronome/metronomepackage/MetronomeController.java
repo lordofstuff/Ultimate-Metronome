675402a04b12a9623bd5531e3cdd8c9fa0d46a91
Stephen Rodriguez <lord.of.stuff@gmail.com>
2014-01-01 16:01:38 -0700

    tried alternate method. still didn't work. trying one more thing.

--- src/com/Stephen/ultimatemetronome/metronomepackage/MetronomeController.java
+++ src/com/Stephen/ultimatemetronome/metronomepackage/MetronomeController.java
@@ -15,7 +15,7 @@ import android.util.Log;
  *
  */
 public class MetronomeController implements Runnable{
-	
+
 	private static final String Tag = "MetronomeController";
 	//fields
 	//private Song song;
@@ -23,21 +23,21 @@ public class MetronomeController implements Runnable{
 	private MetronomeEvent currentEvent;
 	private Iterator<MetronomeEvent> it;
 	private final MyMetronome met;
-	
-	
+
+
 	//constructor
 	public MetronomeController(Context context, Song song) {
 		//this.song = song;
-		
+
 		it = song.iterator();
 		currentEvent = it.next();
 		measureInCurrentEvent = 1;
-		
+
 		//create a metronome object with the initial values needed.
 		met = new MyMetronome(context, currentEvent.tempo, currentEvent.volume, currentEvent.pattern, currentEvent.beat, Sounds.set1, song, this);
 	}
-	
-	
+
+
 	//methods
 
 	/**
@@ -45,13 +45,17 @@ public class MetronomeController implements Runnable{
 	 * increments measure numbers and events, including updating the parameters of the metronome as necessary.
 	 */
 	@Override
-    public void run() {
-	    //gets called every time the metronome finishes a measure
+	public void run() {
+		//gets called every time the metronome finishes a measure
 		//thus, it will increment the measure number and change values for a different event when needed.
-		Log.v(Tag, "Finished measure " + measureInCurrentEvent + "/" + currentEvent.repeats);
+		//Log.v(Tag, "Finished measure " + measureInCurrentEvent + "/" + currentEvent.repeats);
 		//cases: it is just a new measure in the set
 		if (measureInCurrentEvent < currentEvent.repeats) {
 			measureInCurrentEvent++;
+			met.updated = true;
+			synchronized(met.metLock) {
+				met.metLock.notify();
+			}
 		}
 		//it is the end of an event and another follows
 		else if (it.hasNext()) {
@@ -61,26 +65,30 @@ public class MetronomeController implements Runnable{
 		}
 		//it is the end of the song. 
 		else {
-			//TODO stop them?
+
 			met.finish();
 		}
-    }
-	
+	}
+
 	private void updateMet() {
 		Log.d(Tag, "switching events");
 		met.tempo = currentEvent.tempo;
 		met.pattern = currentEvent.pattern;
 		met.volume = currentEvent.volume;
-		
+
 		met.beat = currentEvent.beat;
 		//met.currentBeat = 0;
+		met.updated = true;
+		synchronized(met.metLock) {
+			met.metLock.notify();
+		}
 	}
-	
+
 	public void startMet() {
 		Log.d(Tag, "Starting metronome");
 		met.start(); //FIXME
 		//new Thread(met).start();
-		
+
 	}
 
 }
--- src/com/Stephen/ultimatemetronome/metronomepackage/MyMetronome.java
+++ src/com/Stephen/ultimatemetronome/metronomepackage/MyMetronome.java
@@ -41,7 +41,7 @@ class MyMetronome implements Runnable{
 	private final short[] secondarySoundData;
 	private final AudioTrack track;
 	boolean playing = false;
-	
+
 	//debug things:
 	private static final String Tag = "MyMetronome";
 
@@ -71,6 +71,8 @@ class MyMetronome implements Runnable{
 	Song song;
 	MetronomeController mc;
 	private boolean finishQueue;
+	boolean updated = true;
+	Object metLock = new Object();
 
 	//constructor(s)
 	/**
@@ -132,8 +134,18 @@ class MyMetronome implements Runnable{
 		int frames_since_played = interval_in_frames;
 
 		while (playing) {
+			while (!updated) {
+				synchronized(metLock) {
+					try {
+						metLock.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
 			interval_in_frames = (int) (60 * SAMPLE_RATE / (tempo * beat)); // Recalculate in case tempo changed
-			
+
 			if (frames_since_played >= interval_in_frames) {
 				writeNextBeatOfPattern();
 				frames_since_played = primarySoundData.length;
@@ -143,7 +155,7 @@ class MyMetronome implements Runnable{
 				// Rest for a full write chunk or until the next click needs to play, whichever is less.
 				int rest_length_in_frames = Math.min(frames_left_to_wait, WRITE_CHUNK_IN_FRAMES);
 				track.write(new short[rest_length_in_frames], 0, rest_length_in_frames);
-
+				Log.d(Tag, "wrote silence in " + rest_length_in_frames);
 				frames_since_played += rest_length_in_frames;
 			}
 		}
@@ -152,33 +164,33 @@ class MyMetronome implements Runnable{
 		if (finishQueue) {
 			try {
 				Log.v(Tag, "Finishing Queue");
-	            Thread.sleep(BUFFER_SIZE / WRITE_CHUNK_IN_FRAMES * 200);
-            } catch (InterruptedException e) {
-	            // TODO Auto-generated catch block
-	            e.printStackTrace();
-            }
+				Thread.sleep(BUFFER_SIZE / WRITE_CHUNK_IN_FRAMES * 200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		Log.v(Tag, "Stopping AudioTrack, releasing.");
 		track.stop();
 		track.release();
 	}
-	
+
 	public void start() {
-	    playing = true;
+		playing = true;
 
-	    new Thread(this).start();
-	  }
+		new Thread(this).start();
+	}
 
-	  /**
-	   * Stops the metronome once it finishes playing what is already queued if it was running.
-	   */
-	  public void finish() {
+	/**
+	 * Stops the metronome once it finishes playing what is already queued if it was running.
+	 */
+	public void finish() {
 		finishQueue = true;
-	    playing = false;
-	    
-	  }
+		playing = false;
 
-	
+	}
+
+
 	private void writeNextBeatOfPattern() {
 		if (pattern[currentBeat] == 1) {
 			track.write(primarySoundData, 0, primarySoundData.length);
@@ -199,9 +211,10 @@ class MyMetronome implements Runnable{
 		//adapted to allow for a pattern length that changes. count starts on 0.
 		if (currentBeat + 1 >= pattern.length) {
 			currentBeat = 0; 
+			updated = false;
 			new Thread(mc).start(); //inform the controller that it has finished a measure
-			
 		}
+
 		else {
 			currentBeat++;
 		}
