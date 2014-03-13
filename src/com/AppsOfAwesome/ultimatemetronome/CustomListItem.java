aa84aef03ef476c349fea3f4d9d6274a7213c818
Stephen Rodriguez <lord.of.stuff@gmail.com>
2014-03-12 21:29:15 -0400

    worked on system to include icons and text for different pulse values.

--- src/com/AppsOfAwesome/ultimatemetronome/CustomListItem.java
+++ src/com/AppsOfAwesome/ultimatemetronome/CustomListItem.java
@@ -1,7 +1,38 @@
 package com.AppsOfAwesome.ultimatemetronome;
 
-public abstract class CustomListItem<T> {
-	private int graphicId; //0 for no graphic
-	private String text;
+public class CustomListItem<T> {
+	int graphicId; //0 for no graphic
+	int textId;
+
+	public static final int quarter = 0;
+	public static final int eighth = 1;
+	public static final int sixteenth = 2;
+	public static final int thirty_second = 3;
+	public static final int half = 4;
+
+	public CustomListItem(int type) {
+		switch (type) {
+		case quarter:
+			graphicId = R.drawable.ic_action_quarter_up;
+			textId = R.string.um_quarter;
+			break;
+		case eighth:
+			graphicId = R.drawable.ic_action_eighth_up;
+			textId = R.string.um_eighth;
+			break;
+		case sixteenth:
+			graphicId = R.drawable.ic_action_sixteenth_up;
+			textId = R.string.um_eighth;
+			break;
+		case thirty_second:
+			//graphicId = R.drawable.ic_action_thirtysecond_up;
+			textId = R.string.um_thirtysecond;
+			break;
+		case half:
+			graphicId = R.drawable.ic_action_half_up;
+			textId = R.string.um_half;
+			break;
+		}
+	}
 
 }
